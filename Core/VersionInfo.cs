7074c96758655c911f948a368bcfc9f0c2085758
MC J <jmc8@qq.com>
2026-07-11 20:04:40 +0800

    发布页面

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,6 +3,6 @@ namespace LogConsole.Core;
 public static class VersionInfo
 {
     public const string Name = "LogConsole";
-    public const string Version = "1.1.0";
+    public const string Version = "1.2.1";
     public static string Tag => $"[{Name} v{Version}]";
 }
